4784edfb45d070ede7c1a6a2609325f1a7aa96b7
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-19 22:38:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,19 @@
-class Person 
+class Person
   def name=(name)
     @name = name 
-end   
+end
 
+def name 
+  @name
+end 
+
+def job=(job)
+  @job= job
+end
+
+def job
+  @job
+end
+
+end 
 #You'll be teaching Person about their jobs through two methods, #job, and #job=
